56e5b9c4159eb8b6e26e6e46cb67ae6c1032baaa
Dmitry <dimabulan@yandex.ru>
2021-08-20 17:28:06 +0300

    fdf

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Integer i1 = 10;
+        Integer i2=10;
+
+        Double d1=10d;
+        Double d2=10d;
+        System.out.println(i1==i2);
+        System.out.println(d1==2);
+    }
+}
