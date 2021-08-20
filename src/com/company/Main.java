4fc44ede16e7ec960fb68ad5dd779acd6845cb4a
Dmitry <dimabulan@yandex.ru>
2021-08-20 17:29:30 +0300

    erwerw

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Integer i1 = 10;
         Integer i2=10;
-
+ //ggggggggggggggg
         Double d1=10d;
         Double d2=10d;
         System.out.println(i1==i2);
